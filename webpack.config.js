712e5c915bdb2e82222adba1f550226ad839e16a
Negrusa, Dan <dan.negrusa@experian.com>
2020-04-19 20:43:06 +0200

    folder structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-	entry: './client.js',
+	entry: './src/sockets-service.js',
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, './dist'),
